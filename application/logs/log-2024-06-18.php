07da476a1201e0b3862a63344ea2be7443628cf8
Manuel Morales <manuelmopur@gmail.com>
2024-06-20 17:34:40 +0000

    Cambios en la aplicacion ya funcionando en red local e internet con direcciones ip

---
+++ application/logs/log-2024-06-18.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-06-18 23:58:33 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-18 23:58:33 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:33 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:44 --> Severity: Notice --> Undefined property: stdClass::$id_alumno /data-app/ExitusFactura/ExitusFactura24/application/views/admin/alumnos/index.php 405
+ERROR - 2024-06-18 23:58:57 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-18 23:58:57 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-18 23:58:57 --> 404 Page Not Found: Assets/assets
---
+++ application/logs/log-2024-06-19.php
@@ -0,0 +1,102 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-06-19 11:38:35 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Network is unreachable /data-app/ExitusFactura/ExitusFactura24/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-06-19 11:38:35 --> Unable to connect to the database
+ERROR - 2024-06-19 11:39:12 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Network is unreachable /data-app/ExitusFactura/ExitusFactura24/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-06-19 11:39:12 --> Unable to connect to the database
+ERROR - 2024-06-19 12:48:22 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Network is unreachable /data-app/ExitusFactura/ExitusFactura24/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-06-19 12:48:22 --> Unable to connect to the database
+ERROR - 2024-06-19 20:09:47 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:09:47 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:47 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:54 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:09:54 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:09:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:55 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:09:55 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:47 --> Severity: Notice --> Undefined property: stdClass::$id_alumno /data-app/ExitusFactura/ExitusFactura24/application/views/admin/alumnos/index.php 405
+ERROR - 2024-06-19 20:11:50 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:11:50 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:11:54 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:11:54 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:11:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:35:16 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:16 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:21 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:35:21 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 20:35:21 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:22 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:22 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:22 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:22 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 20:35:22 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:27:54 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:27:54 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> Severity: Notice --> Undefined property: stdClass::$id_alumno /data-app/ExitusFactura/ExitusFactura24/application/views/admin/alumnos/index.php 405
+ERROR - 2024-06-19 21:28:04 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:28:04 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:28:04 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> Severity: Notice --> Undefined property: stdClass::$id_alumno /data-app/ExitusFactura/ExitusFactura24/application/views/admin/alumnos/index.php 405
+ERROR - 2024-06-19 21:49:29 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:49:29 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 21:49:29 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 22:17:00 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:17:00 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> Severity: Notice --> Undefined property: stdClass::$id_alumno /data-app/ExitusFactura/ExitusFactura24/application/views/admin/alumnos/index.php 405
+ERROR - 2024-06-19 22:19:52 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 22:19:52 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 22:19:52 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> Severity: Notice --> Undefined variable: user_info_logged_in /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 23:41:39 --> Severity: Notice --> Trying to get property 'avatar' of non-object /data-app/ExitusFactura/ExitusFactura24/application/views/body.php 81
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
+ERROR - 2024-06-19 23:41:39 --> 404 Page Not Found: Assets/assets
